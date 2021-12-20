2b865fd009cd4117abe24e7a2c0580aa98c37976
Mahmoud Miri <sm_miri83@yahoo.com>
2021-12-20 01:32:02 +0000

    my implementation of useReducer from https://www.robinwieruch.de/react-usereducer-hook/

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer } from "react";
+
+const initialTodos = [
+  {
+    id: "a",
+    task: "Learn React",
+    complete: false,
+  },
+  {
+    id: "b",
+    task: "Learn Firebase",
+    complete: false,
+  },
+];
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "DO_TODO":
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, complete: true } : todo
+      );
+    case "UNDO_TODO":
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, complete: false } : todo
+      );
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [todos, dispatch] = useReducer(todoReducer, initialTodos);
+
+  const handleToggle = (todo) => {
+    dispatch({
+      type: todo.complete ? "UNDO_TODO" : "DO_TODO",
+      id: todo.id,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <input
+            type="checkbox"
+            checked={todo.complete}
+            onChange={() => handleToggle(todo)}
+          />
+          {todo.task}
+        </div>
+      ))}
     </div>
   );
 }
